8dba836a1d81a43e43b23ea25283bfb468692360
ktai98 <tai98.k@gmail.com>
2024-06-26 02:36:47 +0900

    Implement TurnComponent

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <v-app-bar>
+        <v-app-ver-title>
+          学マス計算機
+        </v-app-ver-title>
+      </v-app-bar>
+      <TurnComponent/>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TurnComponent from './components/TurnComponent.vue';
 
 export default {
   name: 'App',
 
   components: {
     HelloWorld,
+    TurnComponent,
   },
 
   data: () => ({
